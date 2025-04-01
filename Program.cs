2c64f818cdbf2eb3637caafd28545cd8a35ea3bf
Halfdan <halfdansc@gmail.com>
2025-04-01 10:26:51 +0200

    added db context to the builder and configured it to use postgreSql in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using super_hero.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// configure to use PostgreSQL
+builder.Services.AddDbContext<SuperheroDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
